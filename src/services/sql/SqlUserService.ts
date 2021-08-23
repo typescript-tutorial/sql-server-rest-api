21db1a38ce60fa412acbc5fbaa3f1a91c0d26611
Anh Le <lhanhfire1999@gmail.com>
2021-08-23 11:07:38 +0700

    add upsert using merge

--- src/services/sql/SqlUserService.ts
+++ src/services/sql/SqlUserService.ts
@@ -29,12 +29,24 @@ export class SqlUserService implements UserService {
     return queryOne(this.db, 'select * from users where id = @0', [id]);
   }
   insert(user: User): Promise<number> {
-    const upSert = `if exists (select * from users where id = @0)
-    update users set username = @1, email = @2, phone = @3, dateOfBirth = @4 where id = @0
-    else
-    insert into users (id, username, email, phone, dateOfBirth) values (@0, @1, @2, @3, @4)`;
-    return exec(this.db, upSert,
-     [user.id, user.username, user.email, user.phone, user.dateOfBirth]);
+    // const upSert = `if exists (select * from users where id = @0)
+    // update users set username = @1, email = @2, phone = @3, dateOfBirth = @4 where id = @0
+    // else
+    // insert into users (id, username, email, phone, dateOfBirth) values (@0, @1, @2, @3, @4)`;
+    const upSert2 = `MERGE users AS f
+    USING (SELECT @0 AS ID, @1 as USERNAME, @2 as EMAIL, @3 as PHONE, @4 as DATEOFBIRTH ) AS new_user
+    ON f.id = new_user.ID
+    WHEN MATCHED THEN
+      UPDATE SET 
+      f.username = new_user.USERNAME,
+      f.email = new_user.EMAIL,
+      f.phone = new_user.PHONE,
+      f.dateOfBirth = new_user.DATEOFBIRTH
+    WHEN NOT MATCHED THEN
+      INSERT (id, username, email, phone, dateOfBirth)
+      VALUES (new_user.ID, new_user.USERNAME, new_user.EMAIL, new_user.PHONE, new_user.DATEOFBIRTH);`;
+    return exec(this.db, upSert2,
+     [user.id, user.username, user.email, user.phone, user.dateOfBirth])
   }
   insertMany(users: User[]): Promise<number> {
     const query = `insert into users (id, username, email, dateOfBirth) values (@0, @1, @2, @3)`;
